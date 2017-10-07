a06400b91abae43f0fb716e3c0a181cb2ffaa374
danny <danny@US02.fritz.box>
2017-10-07 15:46:47 +0200

    Made Threads

--- src/Controller/FileControllerThread.java
+++ src/Controller/FileControllerThread.java
@@ -1,5 +1,15 @@
 package Controller;
 
-public class FileControllerThread {
+public class FileControllerThread extends Thread {
+
+	public void run() {
+		while (true) {
+			try {
+				sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
--- src/Daemon/Daemon.java
+++ src/Daemon/Daemon.java
@@ -1,9 +1,11 @@
 package Daemon;
 
+import Controller.FileControllerThread;
+
 public class Daemon {
 
 	public static void main(String[] args) {
-
+		(new FileControllerThread()).start();
 	}
 
 }
